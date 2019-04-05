cc091ecefe58636d95e22ce1de9fd18192c7b66c
Alberto Carreras <a.carreras.c@gmail.com>
2019-04-05 15:46:14 -0400

    added style to validation errors in form

--- src/RequestForm.js
+++ src/RequestForm.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-let map = {}
-
 class RequestForm extends Component {
 
   state = {
@@ -11,32 +9,65 @@ class RequestForm extends Component {
     serviceRequest: undefined,
     bodyRequest: "",
     checkboxTerms: false,
-    error: null
+    serverError: null,
+    validationError: {}
   }
 
-  displayMessage = (input) => {
-    switch(map[input]) {
-      case "firstName":
-        return "This field cannot be blank."
-      default:
-        return "required"
+  validateFields = () => {
+    var allowSubmission = true;
+    var validationError = {}
+    if (this.state.firstName === "" || this.state.lastName === "") {
+      allowSubmission = false;
+      validationError["name"] = "First Name and Last Name cannot be blank."
     }
+    
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(this.state.email)) {
+      allowSubmission = false;
+      validationError["email"] = "Invalid email address format."
+    }
+    
+    if ((this.state.serviceRequest === undefined)) {
+      allowSubmission = false;
+      validationError["service"] = "A service type must be selected."
+    }
+    
+    if ((this.state.bodyRequest === "")) {
+      allowSubmission = false;
+      validationError["description"] = "A description must be provided."
+    }
+
+    if (!(this.state.checkboxTerms)) {
+      allowSubmission = false;
+      validationError["checkbox"] = "You must accept to terms and conditions."
+    }
+
+    return allowSubmission
+    ? this.handleSubmit()
+    : this.setState({validationError: validationError})
   }
 
-  displayError = () => {
-    const error = this.state.error 
+  displayServerError = () => {
+    const error = this.state.serverError 
     return error
       ? <div>{error.message.message}</div>
       : null
   }
 
+  displayValidationError = (field) => {
+    const { validationError } = this.state
+
+    if (validationError !== {} && validationError[field]) {
+      return <div>{validationError[field]}</div>
+    }
+    return <div>required</div>
+  }
+
   handleChange = (event) => {
     const {target} = event
     const {name, value} = target
-
     name === "checkboxTerms" 
     ? this.setState({
-        [name]: !value,
+      checkboxTerms: !this.state.checkboxTerms
       })
     : this.setState({
         [name]: value,
@@ -79,8 +110,8 @@ class RequestForm extends Component {
         this.props.saveApiResponse(await response.json())
     } 
     else {
-        this.setState({  error: { code: valid.errorCode, 
-                                  message: await response.json() } })
+        this.setState({  serverError: { code: valid.errorCode, 
+                                        message: await response.json() } })
       }
   }
 
@@ -97,64 +128,80 @@ class RequestForm extends Component {
   }
   
   render() {
+
+    const divStyleError = {
+      border: '1px solid red',
+      color: 'red'
+    };
+
     return (
       <div > 
-        <h1>{this.displayError()}</h1>  
+        <h1>{this.displayServerError()}</h1>  
         <div>New Assistance Request</div>
         <div className="form">
               
           <input 
-            type="text" 
-            placeholder="First Name"
+            type="text"
             name="firstName"
+            style={this.state.validationError['name'] ? divStyleError : null} 
+            placeholder="First Name"
             onChange={ this.handleChange }
             value={this.state.firstName} />
 
           <input 
             type="text" 
-            placeholder="Last Name"
             name="lastName"
+            style={this.state.validationError['name'] ? divStyleError : null} 
+            placeholder="Last Name"
             onChange={ this.handleChange }
             value={this.state.lastName} />
+          {this.displayValidationError("name")}
 
           <input 
             type="email" 
-            placeholder="Email Address"
             name="email"
+            style={this.state.validationError['email'] ? divStyleError : null} 
+            placeholder="Email Address"
             onChange={ this.handleChange }
             value={this.state.email} />
-            
+          {this.displayValidationError("email")}
+
           <select
             name="serviceRequest"
+            style={this.state.validationError['service'] ? divStyleError : null} 
             onChange={ this.handleChange }
             value={this.state.serviceRequest}>
               <option value="">Select Service Type</option>
               {this.buildServiceOptionList()}
           </select>
+          {this.displayValidationError("service")}
           
           <textarea 
+            name="bodyRequest"
+            style={this.state.validationError['description'] ? divStyleError : null} 
             placeholder="Provide information about your request"
             rows="5" 
             cols="20"
-            name="bodyRequest"
             onChange={ this.handleChange }
             value={this.state.bodyRequest}>
           </textarea>
+          {this.displayValidationError("description")}
 
           <input 
-            type="checkbox" 
-            name="checkboxTerms" 
+            type="checkbox"
+            name="checkboxTerms"  
             id="checkboxTerms"
             onChange={ this.handleChange }
-            value={this.state.checkboxTerms}/>
+            checked={this.state.checkboxTerms}/>
           <label
+            style={this.state.validationError['checkbox'] ? divStyleError : null} 
             htmlFor="checkboxTerms">I hereby accept the terms of service for THE NETWORK and the privacy policy.</label>
 
           <input 
             className="button"
             type="button" 
             value="Get Assistance" 
-            onClick={this.handleSubmit}/>
+            onClick={this.validateFields}/>
           </div>
       </div>
     );
